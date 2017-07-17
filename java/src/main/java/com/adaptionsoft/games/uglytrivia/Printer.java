142ff6c1df93f193ca0b796684da610e63343118
dsucksto <dsucksto@thoughtworks.com>
2017-07-17 11:08:09 -0500

    Add printer and test

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Printer.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Printer.java
@@ -1,4 +1,16 @@
 package com.adaptionsoft.games.uglytrivia;
 
-public class Printer {
+import java.io.PrintStream;
+
+class Printer {
+    private PrintStream printStream;
+
+
+    Printer(PrintStream printStream) {
+        this.printStream = printStream;
+    }
+
+    void printLine(String toPrint) {
+        printStream.println(toPrint);
+    }
 }
--- java/src/test/java/com/adaptionsoft/games/uglytrivia/PrinterTest.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/PrinterTest.java
@@ -1,4 +1,28 @@
 package com.adaptionsoft.games.uglytrivia;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class PrinterTest {
+
+    private PrintStream mockedPrintStream;
+    private Printer printer;
+
+    @Before
+    public void setup() {
+        mockedPrintStream = mock(PrintStream.class);
+        printer = new Printer(mockedPrintStream);
+    }
+
+    @Test
+    public void shouldWriteToPrintStream() {
+       printer.printLine("This is what I want to print");
+
+       verify(mockedPrintStream).println("This is what I want to print");
+    }
 }
